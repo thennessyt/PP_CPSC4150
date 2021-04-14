c3c3f1b58064b21ca262efd0aa0fe77f9df6254d
Teresa Chen <tlchen@clemson.edu>
2021-04-13 22:48:22 -0400

    Opening menu and visit menu update.

--- ePITAPH/app/src/main/java/cpsc4150/hw2/epitaph/OpeningMenuActivity.java
+++ ePITAPH/app/src/main/java/cpsc4150/hw2/epitaph/OpeningMenuActivity.java
@@ -1,14 +1,48 @@
 package cpsc4150.hw2.epitaph;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class OpeningMenuActivity extends AppCompatActivity {
+public class OpeningMenuActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_menu);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.activity_opening_menu);
+
+        if (fragment == null)
+        {
+            fragment = new OpeningMenuFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.activity_opening_menu, fragment)
+                    .commit();
+        }
+    }
+
+    // ------------ BUTTON ON-CLICK
+    //create a new memorial
+    public void onClickCreate()
+    {
+        //Start CreateMemorialActivity
+        Intent intent = new Intent(this, CreateMemorialActivity.class);
+        startActivity(intent);
+    }
+
+
+    //visit memorial menu
+    public void onClickVisit()
+    {
+        //Start VisitMenuActivity
+        Intent intent = new Intent(this, VisitMenuActivity.class);
+        startActivity(intent);
     }
 }
--- ePITAPH/app/src/main/java/cpsc4150/hw2/epitaph/OpeningMenuFragment.java
+++ ePITAPH/app/src/main/java/cpsc4150/hw2/epitaph/OpeningMenuFragment.java
@@ -1,5 +1,6 @@
 package cpsc4150.hw2.epitaph;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,56 +9,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OpeningMenuFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class OpeningMenuFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OpeningMenuFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OpeningMenuFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OpeningMenuFragment newInstance(String param1, String param2) {
-        OpeningMenuFragment fragment = new OpeningMenuFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class OpeningMenuFragment extends Fragment
+{
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_opening_menu, container, false);
     }
