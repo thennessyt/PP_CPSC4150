0d4e9e321211cfda1c89f2ed1d7078bbfa20398d
Teresa Chen <tlchen@clemson.edu>
2021-04-18 15:10:02 -0400

    Copy ID from memorial page.

--- ePITAPH/app/src/main/java/cpsc4150/epitaph/activities/MemorialViewActivity.java
+++ ePITAPH/app/src/main/java/cpsc4150/epitaph/activities/MemorialViewActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import cpsc4150.epitaph.fragments.MemorialViewFragment;
 import cpsc4150.epitaph.R;
@@ -12,6 +16,7 @@ import cpsc4150.epitaph.R;
 public class MemorialViewActivity extends AppCompatActivity
 {
     public static final String EXTRA_MEMORIAL_ID = "cpsc4150.epitaph.memorial_id";
+    private int memorialID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,5 +34,16 @@ public class MemorialViewActivity extends AppCompatActivity
                     .add(R.id.activity_memorial_view, fragment)
                     .commit();
         }
+
+        Bundle extra = getIntent().getExtras();
+        memorialID = extra.getInt(MemorialViewActivity.EXTRA_MEMORIAL_ID);
+    }
+
+    public void onCopyClick(View view)
+    {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("label", String.valueOf(memorialID));
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(this, "Memorial ID copied!", Toast.LENGTH_LONG).show();
     }
 }
