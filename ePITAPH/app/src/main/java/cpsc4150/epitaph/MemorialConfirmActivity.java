882c566e6a985e337f4bf899e44aa6ddc6c7f0a7
thennessyt <thennessyt@gmail.com>
2021-04-18 11:09:32 -0400

    adding confirmation page

---
+++ ePITAPH/app/src/main/java/cpsc4150/epitaph/MemorialConfirmActivity.java
@@ -0,0 +1,14 @@
+package cpsc4150.epitaph;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemorialConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memorial_confirm);
+    }
+}
