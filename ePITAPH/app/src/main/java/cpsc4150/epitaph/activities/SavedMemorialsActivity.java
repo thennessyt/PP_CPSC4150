5dc247e66ce6c92b3440a9c854e6da92cecf9572
Teresa Chen <tlchen@clemson.edu>
2021-04-18 15:00:39 -0400

    Removed local memorial search, implemented saved memorials.

--- ePITAPH/app/src/main/java/cpsc4150/epitaph/activities/SavedMemorialsActivity.java
+++ ePITAPH/app/src/main/java/cpsc4150/epitaph/activities/SavedMemorialsActivity.java
@@ -2,11 +2,13 @@ package cpsc4150.epitaph.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import cpsc4150.epitaph.R;
+import cpsc4150.epitaph.fragments.SavedMemorialsFragment;
 
-public class SavedMemorialsActivity extends AppCompatActivity
+public class SavedMemorialsActivity extends AppCompatActivity implements SavedMemorialsFragment.OnMemorialSelectedListener
 {
 
     @Override
@@ -14,5 +16,14 @@ public class SavedMemorialsActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_memorials);
+    }
+
+    @Override
+    public void onMemorialSelected(int memorialID)
+    {
+        // Send the band ID of the clicked button to DetailsActivity
+        Intent intent = new Intent(this, MemorialViewActivity.class);
+        intent.putExtra(MemorialViewActivity.EXTRA_MEMORIAL_ID, memorialID);
+        startActivity(intent);
     }
 }
--- ePITAPH/app/src/main/java/cpsc4150/epitaph/fragments/SavedMemorialsFragment.java
+++ ePITAPH/app/src/main/java/cpsc4150/epitaph/fragments/SavedMemorialsFragment.java
@@ -1,17 +1,64 @@
 package cpsc4150.epitaph.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cpsc4150.epitaph.R;
+import cpsc4150.epitaph.models.Memorial;
 
 public class SavedMemorialsFragment extends Fragment
 {
+    public interface OnMemorialSelectedListener
+    {
+        void onMemorialSelected(int memorialID);
+    }
+
+    @Override
+    public void onAttach(Context context)
+    {
+        super.onAttach(context);
+        if (context instanceof SavedMemorialsFragment.OnMemorialSelectedListener)
+        {
+            mListener = (SavedMemorialsFragment.OnMemorialSelectedListener) context;
+        }
+        else
+        {
+            throw new RuntimeException(context.toString() + " must implement OnMemorialSelectedListener");
+        }
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+        mListener = null;
+    }
+
+    private View.OnClickListener buttonClickListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+            // Notify activity of memorial selection
+            String memorialID = (String) view.getTag();
+            mListener.onMemorialSelected(Integer.parseInt(memorialID));
+        }
+    };
+
+    // Reference to the activity
+    private SavedMemorialsFragment.OnMemorialSelectedListener mListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -24,6 +71,86 @@ public class SavedMemorialsFragment extends Fragment
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_memorials, container, false);
+        View view = inflater.inflate(R.layout.fragment_saved_memorials, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.fragment_saved_memorials);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // Send memorials to recycler view
+        //TODO: GET SAVED MEMORIALS FROM ACCOUNT
+        List<Memorial> list = new ArrayList<>();
+        MemorialAdapter adapter = new MemorialAdapter(list);
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+    private class MemorialHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener
+    {
+        private Memorial memorial;
+
+        private TextView memorialItemName;
+        private TextView memorialItemRange;
+        private TextView memorialItemLocation;
+
+        public MemorialHolder(LayoutInflater inflater, ViewGroup parent)
+        {
+            super(inflater.inflate(R.layout.list_item_memorial, parent, false));
+            itemView.setOnClickListener(this);
+            memorialItemName = itemView.findViewById(R.id.memorialItemName);
+            memorialItemRange = itemView.findViewById(R.id.memorialItemRange);
+            memorialItemLocation = itemView.findViewById(R.id.memorialItemLocation);
+        }
+
+        public void bind(Memorial memorial)
+        {
+            this.memorial = memorial;
+            memorialItemName.setText(memorial.getName());
+            memorialItemRange.setText(memorial.getByear() + " - " + memorial.getDyear());
+            String locations = "";
+            for(int i = 0; i < memorial.getLocations().size(); i++)
+            {
+                locations += memorial.getLocations().get(i) + "\n";
+            }
+            memorialItemLocation.setText(locations);
+        }
+
+        @Override
+        public void onClick(View view)
+        {
+            // Tell ListActivity what memorial was clicked
+            mListener.onMemorialSelected(memorial.getId());
+        }
+    }
+
+    private class MemorialAdapter extends RecyclerView.Adapter<SavedMemorialsFragment.MemorialHolder>
+    {
+        private List<Memorial> memorials;
+
+        public MemorialAdapter(List<Memorial> memorials)
+        {
+            this.memorials = memorials;
+        }
+
+        @Override
+        public SavedMemorialsFragment.MemorialHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new SavedMemorialsFragment.MemorialHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(SavedMemorialsFragment.MemorialHolder holder, int position)
+        {
+            Memorial memorial = memorials.get(position);
+            holder.bind(memorial);
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return memorials.size();
+        }
     }
 }
