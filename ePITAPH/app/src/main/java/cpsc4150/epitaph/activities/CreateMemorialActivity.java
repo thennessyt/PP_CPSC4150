6f21909904360a15c34adf0db506a7064f102f8a
Teresa Chen <tlchen@clemson.edu>
2021-04-18 20:32:01 -0400

    Picture taking in CreeateMemorial.

--- ePITAPH/app/src/main/java/cpsc4150/epitaph/activities/CreateMemorialActivity.java
+++ ePITAPH/app/src/main/java/cpsc4150/epitaph/activities/CreateMemorialActivity.java
@@ -7,21 +7,29 @@
 
 package cpsc4150.epitaph.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
 import android.location.*;
 import android.widget.Toast;
@@ -31,13 +39,19 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Vector;
 
 import cpsc4150.epitaph.EpitaphDatabase;
 import cpsc4150.epitaph.fragments.CreateMemorialFragment;
 import cpsc4150.epitaph.R;
+import cpsc4150.epitaph.models.Account;
 import cpsc4150.epitaph.models.Comment;
+import cpsc4150.epitaph.models.ImageContribution;
 import cpsc4150.epitaph.models.Memorial;
 
 public class CreateMemorialActivity extends AppCompatActivity
@@ -47,6 +61,13 @@ public class CreateMemorialActivity extends AppCompatActivity
     Location myLoc = null;
 
     private EpitaphDatabase db;
+
+    private final int REQUEST_TAKE_PHOTO = 1;
+
+    private File mPhotoFile;
+    private Bitmap bitmap;
+
+    int accountID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -59,6 +80,8 @@ public class CreateMemorialActivity extends AppCompatActivity
 
         //Initialize database
         db = EpitaphDatabase.getInstance(getApplicationContext());
+        Bundle extra = getIntent().getExtras();
+        accountID = extra.getInt(Account.EXTRA_ACCOUNT_ID);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         Fragment fragment = fragmentManager.findFragmentById(R.id.activity_create_memorial);
@@ -73,7 +96,8 @@ public class CreateMemorialActivity extends AppCompatActivity
     }
 
     //if they want from their location, get the location
-    public void onClickMyLocation(View view){
+    public void onClickMyLocation(View view)
+    {
         //make the submit button unclickable
         Button submit = findViewById(R.id.btn_creatememorial);
         submit.setClickable(false);
@@ -182,10 +206,71 @@ public class CreateMemorialActivity extends AppCompatActivity
         db.memorialDao().insertMemorial(myMem);
         Toast.makeText(this, "Memorial created!", Toast.LENGTH_LONG).show();
 
+        ImageContribution imageContribution = new ImageContribution(bitmap, accountID, myMem.getId());
+        db.imageContributionDao().insertImage(imageContribution);
+
         //Start MemorialViewActivity
         Intent intent = new Intent(this, MemorialViewActivity.class);
         intent.putExtra(MemorialViewActivity.EXTRA_MEMORIAL_ID, myMem.getId());
         startActivity(intent);
 
     }
+
+    public void takePhotoClick(View view)
+    {
+        // Create implicit intent
+        Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (photoCaptureIntent.resolveActivity(getPackageManager()) != null)
+        {
+            // Create the File where the photo should go
+            try
+            {
+                mPhotoFile = createImageFile();
+            }
+            catch (IOException ex)
+            {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+
+            // If the File was successfully created, start camera app
+            if (mPhotoFile != null)
+            {
+                // Create content URI to grant camera app write permission to photoFile
+                Uri photoUri = FileProvider.getUriForFile(this,
+                        "cpsc4150.epitaph.fileprovider",
+                        mPhotoFile);
+
+                // Add content URI to intent
+                photoCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+
+                // Start camera app
+                startActivityForResult(photoCaptureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK)
+        {
+            String filePath = mPhotoFile.getPath();
+            bitmap = BitmapFactory.decodeFile(filePath);
+        }
+    }
+
+    private File createImageFile() throws IOException
+    {
+        // Create a unique image filename
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        String imageFilename = "photo_" + timeStamp + ".jpg";
+
+        // Get file path where the app can save a private image
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return new File(storageDir, imageFilename);
+    }
+
 }
